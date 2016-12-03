678e98ae5a4e01d8701ea1943412e52d9a77c5b2
magic-cube <15619225472@163.com>
2016-12-03 23:48:57 +0800

    模拟洗牌过程

--- src/Test/CollectionsMethods.java
+++ src/Test/CollectionsMethods.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 /**
  * 模拟洗牌过程
+ * Collections.shuffle()的使用
  * @author hc
  *
  */
@@ -19,19 +20,27 @@ public class CollectionsMethods {
 		//洗牌，随机排序
 		Collections.shuffle(cards);
 		
-		List<Integer> list1 = new ArrayList<Integer>();
-		List<Integer> list2 = new ArrayList<Integer>();
-		List<Integer> list3 = new ArrayList<Integer>();
+		List<Integer> p1 = new ArrayList<Integer>();
+		List<Integer> p2 = new ArrayList<Integer>();
+		List<Integer> p3 = new ArrayList<Integer>();
+		//底牌
+		List<Integer> last = new ArrayList<Integer>();
 		//三个人，此时牌已经是乱序，每人依次给一张
 		for(int i=0;i<51;i+=3){
-			list1.add(cards.get(i));
-			list2.add(cards.get(i+1));
-			list3.add(cards.get(i+2));
+			p1.add(cards.get(i));
+			p2.add(cards.get(i+1));
+			p3.add(cards.get(i+2));
 		}
+		//底牌给三张
+		last.add(cards.get(51));
+		last.add(cards.get(52));
+		last.add(cards.get(53));
 		//输出发牌结果
-		System.out.println(list1);
-		System.out.println(list2);
-		System.out.println(list3);
+		System.out.println("P1:"+p1);
+		System.out.println("P2:"+p2);
+		System.out.println("P3:"+p3);
+		System.out.println("底牌为："+last);
+		
 	}
 
 }
