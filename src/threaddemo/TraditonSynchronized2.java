334831eed9d294b4d37183ba7329e080b131898c
magic-cube <15619225472@163.com>
2017-02-14 17:00:45 +0800

    线程同步问题,锁对象,锁方法和锁class对象

---
+++ src/threaddemo/TraditonSynchronized2.java
@@ -0,0 +1,83 @@
+package threaddemo;
+/*
+ * 测试多个线程同时访问一个方法时,由于线程未同步而出现的错误
+ */
+public class TraditonSynchronized2 {
+	/*
+	 * 在静态方法中不能new内部类的实例对象
+	 */
+	public static void main(String[] args) {
+		new TraditonSynchronized2().init();
+	}
+	
+	static class Outputer{
+		public void output(String name){
+			int len=name.length();
+			/*
+			 * 锁name不行,两个线程使用的是同一个对象时,锁对象
+			 */
+			synchronized(Outputer.class){
+				for(int i=0;i<len;i++){
+					System.out.print(name.charAt(i)+" ");
+				}
+			}
+			System.out.println();
+		}
+		
+		public synchronized void output2(String name){
+			int len=name.length();
+			for(int i=0;i<len;i++){
+				System.out.print(name.charAt(i)+" ");
+			}
+			System.out.println();
+		}
+		
+		public static synchronized  void output3(String name){
+			int len=name.length();
+			for(int i=0;i<len;i++){
+				System.out.print(name.charAt(i)+" ");
+			}
+			System.out.println();
+		}
+	}
+	
+	/*
+	 * 不能在静态方法中new内部类的实例对象
+	 * 内部类可以访问外部类的成员变量,类只有在被实例化后才会为成员变量分配空间
+	 * 而静态方法可以不用去创建类的对象,因而产生了矛盾
+	 * 这时,使用别的方法去完成创建内部类对象的动作,便不会发生错误
+	 */
+	private void init(){
+		final Outputer out=new Outputer();
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				while(true){
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					out.output("zhangxiaoxiang");
+				}
+			}
+		}).start();
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				while(true){
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					out.output3("lihuoming");
+				}
+			}
+		}).start();
+
+	}
+	
+}
